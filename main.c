bda77790168071f5a94dfad95ac33add8089b5ec
Sharfy Adamantine <sadamant@student.42.us.org>
2018-03-30 13:53:28 +0200

    remove draw square

--- main.c
+++ main.c
@@ -13,22 +13,6 @@
 #include "wolf3d.h"
 #include <stdio.h>
 
-void			draw_square(t_env *e)
-{
-	int	x;
-	int	y;
-	int	h;
-
-	x = 350;
-	y = 100;
-	h = 100;
-	while (y++ < 150)
-	{
-		printf("y is %d\n", y);
-		insert_bitmap(e->img, 350, y, COLOR);
-	}
-}
-
 int				main(int argc, char **argv)
 {
 	t_env*		e;
