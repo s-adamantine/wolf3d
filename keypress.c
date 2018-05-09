32025e48fe2ba0e531d19b6d08c8d16252450ab3
Sharfy Adamantine <sadamant@student.42.us.org>
2018-05-09 17:07:12 +0200

    fix oob segfault

--- keypress.c
+++ keypress.c
@@ -12,18 +12,18 @@
 
 #include "wolf3d.h"
 
-static int		insidewall(t_world *world, double x, double y)
-{
-	if (world->map[(int)y / world->tile][(int)x / world->tile] == 'x')
-		return (1);
-	return (0);
-}
+/*
+** check if the player will move out of the map bounds or will collide into a
+** wall
+*/
 
-static int		oob(t_world *world, double x, double y)
+static int		out_of_bounds(t_world *world, double x, double y)
 {
 	if (x < 0 || y < 0)
 		return (1);
 	if (x > (world->w * world->tile) || y > (world->h * world->tile))
+		return (1);
+	if (world->map[(int)y / world->tile][(int)x / world->tile] == 'x')
 		return (1);
 	return (0);
 }
@@ -37,11 +37,9 @@ void			move_player(int keycode, t_env *e)
 	dx *= (keycode == W || keycode == D) ? SPEED : -SPEED;
 	dy = (keycode == W || keycode == S) ? sin(e->p->cov) : cos(e->p->cov);
 	dy *= (keycode == W || keycode == A) ? -SPEED : SPEED;
-	if (!insidewall(e->world, e->p->x + dx, e->p->y) && !oob(e->world, \
-		e->p->x + dx, e->p->y))
+	if (!out_of_bounds(e->world, e->p->x + dx, e->p->y))
 		e->p->x += dx;
-	if (!insidewall(e->world, e->p->x, e->p->y + dy) && !oob(e->world, \
-			e->p->x, e->p->y + dy))
+	if (!out_of_bounds(e->world, e->p->x, e->p->y + dy))
 		e->p->y += dy;
 	render(e);
 }
