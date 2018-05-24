c829f2b1a02718d72571f222bad2542b6b573e28
sadamant <sadamant@student.42.us.org>
2018-05-25 01:31:30 +0200

    refactor in recalculate_movement

--- keypress.c
+++ keypress.c
@@ -4,7 +4,7 @@
 /*   keypress.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                 +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 15:57:42 by sadamant          #+#    #+#             */
 /*   Updated: 2018/03/13 14:55:59 by sadamant         ###   ########.fr       */
 /*                                                                            */
@@ -28,32 +28,36 @@ static int		out_of_bounds(t_world *world, double x, double y)
 	return (0);
 }
 
-int				refresh_screen(t_env *e)
+/*
+** f = forwards/backwards keypresses (W/S) and s = sideways keypresses (A/D)
+*/
+static void		recalculate_movement(t_key *key, t_player *p, t_world *world)
 {
 	double	dx_f;
 	double	dx_s;
 	double	dy_f;
 	double	dy_s;
 
-	dx_f = 0;
-	dx_s = 0;
-	dy_f = 0;
-	dy_s = 0;
-	e->p->v = (e->key->lshift == 1) ? RUN_SPEED : WALK_SPEED;
-	(e->key->w || e->key->s) ? dx_f = cos(e->p->cov) : 0; // up/down
-	(e->key->a || e->key->d) ? dx_s = sin(e->p->cov) : 0; // left/right
-	dx_f *= (e->key->w) ? e->p->v : -e->p->v;
-	dx_s *= (e->key->d) ? e->p->v : -e->p->v;
-	(e->key->w || e->key->s) ? dy_f = sin(e->p->cov) : 0;
-	(e->key->a || e->key->d) ? dy_s = cos(e->p->cov) : 0;
-	dy_f *= (e->key->w) ? -e->p->v : e->p->v;
-	dy_s *= (e->key->a) ? -e->p->v : e->p->v;
-	(e->key->left) ? e->p->cov += 1 * (M_PI / 180) : 0;
-	(e->key->right) ? e->p->cov -= 1 * (M_PI / 180) : 0;
-	if (!out_of_bounds(e->world, e->p->x + dx_f + dx_s, e->p->y))
-		e->p->x += (dx_f + dx_s);
-	if (!out_of_bounds(e->world, e->p->x, e->p->y + dy_f + dy_s))
-		e->p->y += (dy_f + dy_s);
+	p->v = (key->lshift == 1) ? RUN_SPEED : WALK_SPEED;
+	dx_f = (key->w || key->s) ? cos(p->cov) : 0.0;
+	dx_f *= (key->w) ? p->v : -p->v;
+	dx_s = (key->a || key->d) ? sin(p->cov) : 0.0;
+	dx_s *= (key->d) ? p->v : -p->v;
+	dy_f = (key->w || key->s) ? sin(p->cov) : 0.0;
+	dy_f *= (key->w) ? -p->v : p->v;
+	dy_s = (key->a || key->d) ? cos(p->cov) : 0.0;
+	dy_s *= (key->a) ? -p->v : p->v;
+	(key->left) ? p->cov += 1 * (M_PI / 180) : 0;
+	(key->right) ? p->cov -= 1 * (M_PI / 180) : 0;
+	if (!out_of_bounds(world, p->x + dx_f + dx_s, p->y))
+		p->x += (dx_f + dx_s);
+	if (!out_of_bounds(world, p->x, p->y + dy_f + dy_s))
+		p->y += (dy_f + dy_s);
+}
+
+int				refresh_screen(t_env *e)
+{
+	recalculate_movement(e->key, e->p, e->world);
 	render(e);
 	return (0);
 }
